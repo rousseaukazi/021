91c478766e14f6419b46259241654b0ce864ecfe
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-16 09:19:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-title = st.text_input("What's your idea?")
-st.write("The current movie title is", title)
+idea = st.text_input("What's your idea?")
+st.write("Your idea is ", idea)
